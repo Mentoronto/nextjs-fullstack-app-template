d0b5d3ca550f87fbcd8b254a4816a4f3fd76761c
Mentor Berbatovci <mentorberbatov@gmail.com>
2022-11-16 11:03:59 -0500

    feature: button to search utility component

--- components/utility/search/Search.tsx
+++ components/utility/search/Search.tsx
@@ -1,12 +1,21 @@
-export interface ISearch {
-  sampleTextProp: string;
-}
+export interface ISearch {}
 
-const Search: React.FC<ISearch> = ({ sampleTextProp }) => {
+const Search: React.FC<ISearch> = () => {
   return (
-    <div className="bg-gradient-to-r from-cyan-500 to-blue-500">
-      {sampleTextProp}
-    </div>
+    <form className="flex flex-col items-center gap-y-5">
+      <input
+        type="text"
+        className="rounded-full border-2 w-5/6 sm:w-96 h-12 px-3"
+      />
+      <div className="space-x-3">
+        <button type="submit" className="btn-primary">
+          Google Search
+        </button>
+        <button type="submit" className="btn-primary">
+          I&apos;m Feeling Lucky!
+        </button>
+      </div>
+    </form>
   );
 };
 
